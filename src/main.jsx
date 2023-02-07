f696975c098242eaf6767daa3b58dfb87f01e436
Surya-7890 <suryamariappan9108@gmail.com>
2023-02-07 12:38:35 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StaffRegister from './staffRegister';
-import StudentRegister from './studentRegister';
-import Admin from './admin';
-import ViewStudentAttendance from './viewStudentAttendance';
-import SelectRegister from './selectRegister';
-import AdminInfo from './adminInfo';
+import StaffRegister from './staffRegister.jsx';
+import StudentRegister from './studentRegister.jsx';
+import Admin from './admin.jsx';
+import ViewStudentAttendance from './viewStudentAttendance.jsx';
+import SelectRegister from './selectRegister.jsx';
+import AdminInfo from './adminInfo.jsx';
 import './index.css';
 import {BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './home';
+import Home from './home.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
